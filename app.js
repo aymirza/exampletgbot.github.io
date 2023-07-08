ef4cb377ca2f7196caf0293b50c24d8e61398ae2
aymirza <49402749+aymirza@users.noreply.github.com>
2023-07-08 14:49:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand()
+tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2CAB37";
@@ -8,12 +8,12 @@ tg.MainButton.color = "#2CAB37";
 let item = "";
 
 
-let btn1 = document.getElementById("btn1")
-let btn2 = document.getElementById("btn2")
-let btn3 = document.getElementById("btn3")
-let btn4 = document.getElementById("btn4")
-let btn5 = document.getElementById("btn5")
-let btn6 = document.getElementById("btn6")
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 
 btn1.addEventListener("click", function (){
     if(tg.MainButton.isVisible){
