13bdfbbc2c64740034a1cb0cda39eca811bb5b12
aymirza <49402749+aymirza@users.noreply.github.com>
2023-07-08 14:39:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ Telegram.WebApp.onEvent("mainButtonClicked", function (){
 
 let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
-p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name} ${tg.initDataUnsafe.user.user_id}`;
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p)
 
